2df8c81d0a0a47d0695b559e54543301aefcf95d
Isamov Dilshod <killertriller551@gmail.com>
2026-05-05 19:06:21 +0500

    Fix Vercel 500 error: improve entry point and static storage

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techshop.settings.production')
-from techshop.wsgi import application
+import sys
+from django.core.wsgi import get_wsgi_application
+
+# Add current directory to path to ensure techshop package is findable
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techshop.settings.production")
+
+application = get_wsgi_application()
 app = application
--- techshop/settings/base.py
+++ techshop/settings/base.py
@@ -114,7 +114,7 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 # Media files
