f15b5ed36415869ed7e0f442fc32b441e223c467
Isamov Dilshod <killertriller551@gmail.com>
2026-05-06 01:53:03 +0500

    Configure Neon PostgreSQL for production

--- techshop/settings/production.py
+++ techshop/settings/production.py
@@ -7,6 +7,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 if '.vercel.app' not in ALLOWED_HOSTS:
     ALLOWED_HOSTS.append('.vercel.app')
 
+import dj_database_url
+
 # HTTPS & Security
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
@@ -14,3 +16,21 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = False  # Vercel handles SSL termination
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=''),
+        conn_max_age=600,
+        conn_health_checks=True,
+        engine='django.db.backends.postgresql',
+    )
+}
+
+# Use pg8000 as the database driver (better for serverless/Vercel)
+# For now, we use default psycopg2 if available locally
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require',
+}
