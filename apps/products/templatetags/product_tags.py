985199b4f34aba0d445db191f282ddeea9437c61
mirik <mirik@MacBook-Pro-mirik.local>
2026-05-07 18:38:21 +0500

    Fix missing get_categories template tag

--- apps/products/templatetags/product_tags.py
+++ apps/products/templatetags/product_tags.py
@@ -2,6 +2,12 @@ from django import template
 
 register = template.Library()
 
+@register.simple_tag
+def get_categories():
+    from apps.products.models import Category
+    from django.db.models import Count
+    return Category.objects.annotate(num_products=Count('products')).filter(num_products__gt=0)
+
 @register.simple_tag(takes_context=True)
 def translate_url(context, lang_code):
     path = context.get('request').path
